f163ca98c58252e20b14fd9f78724d8af37ea8f8
stevenzimmer <stevenzimmer40@gmail.com>
2016-04-01 09:44:23 -0700

    formvalidation upload

---
+++ formvalidation.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+$("#validation").submit(function(event) {
+	
+	
+	var errmessage = "";
+	event.preventDefault();
+	
+	function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
+if (!isValidEmailAddress($("#email").val())) {
+
+	errmessage = "Please enter valid email address";
+
+}
+
+if (!$.isNumeric($("#phone").val())) {
+
+	errmessage = errmessage+" <br />Please enter valid phone number";
+
+}
+
+if ($("#pass1").val() != $("#pass2").val()) {
+
+	errmessage = errmessage+" <br />Please enter matching passwords";
+
+}
+
+if (errmessage=="") {
+	alert("success");
+} else {
+
+	$("#error").html(errmessage);
+}
+});
